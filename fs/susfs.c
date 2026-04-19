663c98392fd90f64a1283102151a5a8f2df211c0
cyberc3dr <47525358+cyberc3dr@users.noreply.github.com>
2026-04-19 20:23:29 +0000

    susfs: disable avc log spoofing

--- fs/susfs.c
+++ fs/susfs.c
@@ -1129,7 +1129,7 @@ out_copy_to_user:
 #endif // #ifdef CONFIG_KSU_SUSFS_SUS_MAP
 
 /* susfs avc log spoofing */
-extern bool susfs_is_avc_log_spoofing_enabled;
+bool susfs_is_avc_log_spoofing_enabled = false;
 
 void susfs_set_avc_log_spoofing(void __user **user_info) {
 	struct st_susfs_avc_log_spoofing info = {0};
