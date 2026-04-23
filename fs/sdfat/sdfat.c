7c9b6e9acfdf76fb662ed1cec3839112dca577c9
cyberc3dr <47525358+cyberc3dr@users.noreply.github.com>
2026-04-23 10:12:23 +0000

    sdfat: fix compile

--- fs/sdfat/sdfat.c
+++ fs/sdfat/sdfat.c
@@ -162,7 +162,6 @@ static inline void bio_set_dev(struct bio *bio, struct block_device *bdev)
 #endif
 
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 11, 0)
 static int sdfat_getattr(const struct path *path, struct kstat *stat,
 			u32 request_mask, unsigned int query_flags)
 {
@@ -170,14 +169,6 @@ static int sdfat_getattr(const struct path *path, struct kstat *stat,
 
 	return __sdfat_getattr(inode, stat);
 }
-#else /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 11, 0) */
-static int sdfat_getattr(struct vfsmount *mnt, struct dentry *dentry, struct kstat *stat)
-{
-	struct inode *inode = dentry->d_inode;
-
-	return __sdfat_getattr(inode, stat);
-}
-#endif
 
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 10, 0)
